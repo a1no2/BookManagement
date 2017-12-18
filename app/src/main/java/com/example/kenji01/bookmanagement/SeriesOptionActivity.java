d24d21f7db95227bb186d94dce0b87d7ec3fa05e
a1no2 <a1no2jz2l3@gmail.com>
2017-12-18 14:12:43 +0900

    シリーズ名の更新ができるようになりました。

--- app/src/main/java/com/example/kenji01/bookmanagement/SeriesOptionActivity.java
+++ app/src/main/java/com/example/kenji01/bookmanagement/SeriesOptionActivity.java
@@ -1,14 +1,18 @@
 package com.example.kenji01.bookmanagement;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -39,12 +43,10 @@ public class SeriesOptionActivity extends AppCompatActivity {
 
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Item item = (Item)listView.getItemAtPosition(position);
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
                 String str = arr_id.get(position) + "\n" + arr_name.get(position);
                 t(str);
-
-
+                create_alertDialog(position);
             }
         });
 
@@ -82,5 +84,41 @@ public class SeriesOptionActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
 
     }
+
+    private void create_alertDialog(final int i){
+        final EditText editView = new EditText(getApplicationContext());
+        AlertDialog.Builder alertDialog=new AlertDialog.Builder(this);
+        alertDialog.setMessage("シリーズの名前を入力してください。");      //タイトル設定
+        alertDialog.setView(editView);
+        // OK(肯定的な)ボタンの設定
+        alertDialog.setPositiveButton("登録", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // OKボタン押下時の処理
+                ContentValues valuse = new ContentValues();
+                valuse.put(db_helper.SERIES_NAME, editView.getText().toString());
+                try{
+                    db.update(
+                            db_helper.SERIES_TABLE,
+                            valuse,
+                            db_helper.SERIES_ID + " = " + arr_id.get(i),
+                            null
+                    );
+                    setListView();
+                }catch (SQLException e) {
+                    t("SQLException\n" + e);
+                }
+            }
+        });
+        // NG(否定的な)ボタンの設定
+        alertDialog.setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // NGボタン押下時の処理
+            }
+        });
+        alertDialog.show();
+
+
+    }
+
 
 }
